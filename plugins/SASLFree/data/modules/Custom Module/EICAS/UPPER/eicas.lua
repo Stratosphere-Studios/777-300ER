b8b08a82f9c5cd37b737e81cc3ab9c5a8c3240ce
BRUHegg <109680454+BRUHegg@users.noreply.github.com>
2022-09-11 13:42:40 +0300

    FIxed bugs

--- plugins/SASLFree/data/modules/Custom Module/EICAS/UPPER/eicas.lua
+++ plugins/SASLFree/data/modules/Custom Module/EICAS/UPPER/eicas.lua
@@ -107,7 +107,12 @@ function CheckOvht(pump_type, messages) --Returns the second line of the overhea
 			pump_ovht = globalPropertyfae("Strato/777/hydraulics/pump/demand/ovht", i)
 		end
 		if get(pump_ovht) == 1 then
-			msg = "HYD OVERHEAT PRI " .. names[i]
+			msg = ""
+			if pump_type == 1 then
+				msg = "HYD OVERHEAT PRI " .. names[i]
+			else
+				msg = "HYD OVERHEAT DEM " .. names[i]
+			end
 			table.insert(messages, tlen(messages) + 1, msg)
 		end
 	end
@@ -123,7 +128,12 @@ function CheckFail(pump_type, messages)
 			pump_state = globalPropertyiae("Strato/777/hydraulics/pump/demand/state", i)
 		end
 		if get(pump_fail) == 1 and get(pump_state) > 0 then
-			msg = "HYD PRESS PRI " .. names[i]
+			msg = ""
+			if pump_type == 1 then
+				msg = "HYD PRESS PRI " .. names[i]
+			else
+				msg = "HYD PRESS DEM " .. names[i]
+			end
 			table.insert(messages, tlen(messages) + 1, msg)
 		end
 	end
