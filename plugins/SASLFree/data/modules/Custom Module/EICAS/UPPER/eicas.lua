0d3d64c7d77c3a2758f00c058c9dff309fb6916f
BRUHegg <109680454+BRUHegg@users.noreply.github.com>
2022-11-06 19:22:14 +0300

    Added warnings and advisories

--- plugins/SASLFree/data/modules/Custom Module/EICAS/UPPER/eicas.lua
+++ plugins/SASLFree/data/modules/Custom Module/EICAS/UPPER/eicas.lua
@@ -12,26 +12,49 @@ include("misc_tools.lua")
 
 --Finding sim datarefs
 
+--Cockpit controls
+park_brake_valve = globalPropertyi("Strato/777/gear/park_brake_valve")
+throttle_pos = globalPropertyf("sim/cockpit2/engine/actuators/throttle_ratio_all")
+--Electrical
 battery = globalPropertyiae("sim/cockpit2/electrical/battery_on", 1)
 gen_1 = globalPropertyiae("sim/cockpit2/electrical/generator_on", 1)
 gen_2 = globalPropertyiae("sim/cockpit2/electrical/generator_on", 2)
+--Indicators
+ra_pilot = globalPropertyf("sim/cockpit2/gauges/indicators/radio_altimeter_height_ft_pilot")
+ra_copilot = globalPropertyf("sim/cockpit2/gauges/indicators/radio_altimeter_height_ft_copilot")
+cas_pilot = globalPropertyf("sim/cockpit2/gauges/indicators/airspeed_kts_pilot")
+cas_copilot = globalPropertyf("sim/cockpit2/gauges/indicators/airspeed_kts_copilot")
+--Hydraulics
 pressure_L = globalPropertyiae("Strato/777/hydraulics/press", 1)
 pressure_C = globalPropertyiae("Strato/777/hydraulics/press", 2)
 pressure_R = globalPropertyiae("Strato/777/hydraulics/press", 3)
+--Flight controls
 flaps = globalPropertyfae("sim/flightmodel2/wing/flap1_deg", 1)
 flap_handle = globalPropertyf("sim/cockpit2/controls/flap_ratio")
 --Gear positions
 nw_actual = globalPropertyfae("sim/aircraft/parts/acf_gear_deploy", 1)
 mlg_actual_R = globalPropertyfae("sim/aircraft/parts/acf_gear_deploy", 2)
 mlg_actual_L = globalPropertyfae("sim/aircraft/parts/acf_gear_deploy", 3)
+--Operation
+on_ground = globalPropertyi("sim/flightmodel/failures/onground_any")
 
 --Finding own datarefs
 
+fbw_mode = globalPropertyi("Strato/777/fctl/pfc/mode", 1)
+max_allowable = globalPropertyi("Strato/777/fctl/vmax")
+stall_speed = globalPropertyi("Strato/777/fctl/vstall")
+manuever_speed = globalPropertyi("Strato/777/fctl/vmanuever")
 c_time = globalPropertyf("Strato/777/time/current")
 eicas_brake_temp = globalPropertyi("Strato/777/eicas/brake_temp")
 eicas_tire_press = globalPropertyi("Strato/777/eicas/tire_press")
 altn_gear = globalPropertyi("Strato/777/gear/altn_extnsn")
-handle_pos = globalPropertyf("Strato/777/gear/norm_extnsn", 0)
+main_s_locked = globalPropertyi("Strato/777/gear/main_s_locked")
+handle_pos = globalPropertyf("Strato/777/gear/norm_extnsn")
+acc = globalPropertyi("Strato/777/gear/brake_acc_in_use")
+sys_C_press = globalPropertyfae("Strato/777/hydraulics/press", 2)
+sys_R_press = globalPropertyfae("Strato/777/hydraulics/press", 3)
+stab_cutout_C = globalPropertyi("Strato/777/fctl/stab_cutout_C")
+stab_cutout_R = globalPropertyi("Strato/777/fctl/stab_cutout_R")
 
 --Creating datarefs
 
@@ -40,17 +63,32 @@ recall_past = createGlobalPropertyi("Strato/777/eicas/rcl_past", 0)
 canc = createGlobalPropertyi("Strato/777/eicas/canc", 0)
 windows = createGlobalPropertyfa("Strato/777/windows", {0, 0})
 window_target = createGlobalPropertyia("Strato/777/windows_tgt", {0, 0})
+tr_time = createGlobalPropertyf("Strato/777/test/tr_time", 0)
 
 font = loadFont("BoeingFont.ttf")
 
 tmp = 0
 tmp1 = 0
 
+stab_c_past = 0
 flaps_past = 0
 flap_retract_time = -11
 advisories_past = {}
 handle_past = 1
+altn_gear_past = 0
 gear_display_time = -11
+gear_transit_time = -26
+gear_dn = true
+
+function UpdateAdvisorySide(messages, text_L, text_R, text_both, dref_l, dref_r)
+	if round(get(dref_l)) == 1 and round(get(dref_r)) == 0 then
+		table.insert(messages, tlen(messages) + 1, text_L)
+	elseif round(get(dref_l)) == 0 and round(get(dref_r)) == 1 then
+		table.insert(messages, tlen(messages) + 1, text_R)
+	elseif round(get(dref_l)) == 1 and round(get(dref_r)) == 1 then
+		table.insert(messages, tlen(messages) + 1, text_both)
+	end
+end
 
 function UpdateWindows()
 	for i=1,2 do
@@ -106,6 +144,22 @@ function UpdatePress(messages) --Update pressure advisories
 	end
 end
 
+function checkGearDoors()
+	n_not_in_pos = 0
+	if get(c_time) >= gear_transit_time + 26 then
+		for i=2,4 do
+			local door_pos = globalPropertyfae("Strato/777/gear/doors", i)
+			if get(door_pos) ~= 0 then
+				n_not_in_pos = n_not_in_pos + 1
+			end
+		end
+	end
+	if n_not_in_pos > 0 then
+		return 1
+	end
+	return 0
+end
+
 function CheckOvht(pump_type, messages) --Returns the second line of the overheat message
 	names = {"L", "C1", "C2", "R"}
 	ovht = {}
@@ -150,12 +204,12 @@ end
 function UpdateWindowAdvisory(messages)
 	local pos_w_l = globalPropertyfae("Strato/777/windows", 1)
 	local pos_w_r = globalPropertyfae("Strato/777/windows", 2)
-	if get(pos_w_l) > 0 and get(pos_w_r) == 0 then
-		table.insert(messages, tlen(messages) + 1, "WINDOW FLT DECK L")
-	elseif get(pos_w_l) == 0 and get(pos_w_r) > 0 then
-		table.insert(messages, tlen(messages) + 1, "WINDOW FLT DECK R")
-	elseif get(pos_w_l) > 0 and get(pos_w_r) > 0 then
-		table.insert(messages, tlen(messages) + 1, "WINDOWS")
+	UpdateAdvisorySide(messages, "WINDOW FLT DECK L", "WINDOW FLT DECK R", "WINDOWS", pos_w_l, pos_w_r)
+end
+
+function UpdateStabCutoutAdvisory(messages)
+	if get(stab_cutout_C) * get(stab_cutout_R) == 1  then
+		table.insert(messages, tlen(messages) + 1, "STABILIZER CUTOUT")
 	end
 end
 
@@ -167,8 +221,21 @@ function UpdateGearPos()
 		gear_display_time = get(c_time)
 		handle_past = Round(get(handle_pos), 2)
 	end
-	if handle_past == 0 and Round(get(handle_pos), 2) == 1 then
+	if handle_past == 0 and get(handle_pos) ~= 0 then
 		handle_past = 1
+		if get(altn_gear) == 0 then
+			gear_transit_time = get(c_time)
+		end
+	elseif handle_past == 1 and get(handle_pos) ~= 1 and gear_dn == true then
+		if get(altn_gear) == 0 then
+			gear_transit_time = get(c_time)
+		end
+	end
+	if get(altn_gear) ~= altn_gear_past then
+		if get(altn_gear) == 1 then
+			gear_transit_time = get(c_time)
+		end
+		altn_gear_past = get(altn_gear)
 	end
 	if gear_in_pos == true or Round(get(handle_pos), 2) ~= 0 or get(c_time) < gear_display_time + 10 or get(altn_gear) == 1 then
 		if get(altn_gear) == 0 then
@@ -178,9 +245,13 @@ function UpdateGearPos()
 			local text = ""
 			if avg_gear_pos == 0 then
 				text = "UP"
+				gear_dn = false
 			elseif avg_gear_pos == 1 then
 				text = "DOWN"
 				color = {0, 1, 0}
+				gear_dn = true
+			else
+				gear_dn = false
 			end
 			DrawRect(970, 628, 110, 60, 5, color)
 			drawText(font, 1025, 580, "GEAR", 45, false, false, TEXT_ALIGN_CENTER, {0.17, 0.71, 0.83})
@@ -191,6 +262,7 @@ function UpdateGearPos()
 			end
 		else
 			--Advanced status displaying
+			n_gear_dn = 0
 			for i=1,3 do
 				local color = {1, 1, 1}
 				local text = ""
@@ -200,6 +272,7 @@ function UpdateGearPos()
 				elseif get(gear_pos) == 1 then
 					color = {0, 1, 0}
 					text = "DN"
+					n_gear_dn = n_gear_dn + 1
 				end
 				DrawRect(gear_status_pos[i*2-1], gear_status_pos[i*2], 70, 50, 5, color)
 				if text ~= "" then
@@ -209,6 +282,11 @@ function UpdateGearPos()
 				end
 			end
 			drawText(font, 1025, 580, "GEAR", 45, false, false, TEXT_ALIGN_CENTER, {0.17, 0.71, 0.83})
+			if n_gear_dn == 3 then
+				gear_dn = true
+			else
+				gear_dn = false
+			end
 		end
 	end
 end
@@ -234,21 +312,28 @@ function UpdateFlaps() --this is for updating flap position on eicas
 	flap_settings = {0, 1, 5, 15, 20, 25, 30}
 	text = {"F", "L", "A", "P", "S"}
 	detents = {0, 0.17, 0.33, 0.5, 0.67, 0.83, 1}
-	setting = flap_settings[indexOf(detents, get(flap_handle), 1)]
-	if get(flaps) ~= flaps_past then
-		if get(flaps) == 0 then
+	local setting = flap_settings[indexOf(detents, get(flap_handle), 1)]
+	local cur_flap = 0
+	if get(flaps) ~= nil then
+		cur_flap = get(flaps)
+	end
+	if setting == nil then
+		setting = 0
+	end
+	if cur_flap ~= flaps_past then
+		if cur_flap == 0 then
 			flap_retract_time = get(c_time)
 		end
-		flaps_past = get(flaps)
+		flaps_past = cur_flap
 	end
-	if setting > 0 or get(flaps) > 0 or get(c_time) < flap_retract_time + 10 then
-		if math.abs(get(flaps) - setting) > 0.0002 then
+	if setting > 0 or cur_flap > 0 or get(c_time) < flap_retract_time + 10 then
+		if math.abs(cur_flap - setting) > 0.0002 then
 			color = magenta
 		else
 			color = {0, 1, 0}
 		end
 		handle_on_screen = round(530 - tape_height * get(flap_handle)) --position of flap handle on screen
-		flaps_on_screen = round(530 - Flap_pos2Tape(get(flaps), tape_height))
+		flaps_on_screen = round(530 - Flap_pos2Tape(cur_flap, tape_height))
 		DrawRect(1000, 530, 50, tape_height, 3, {1, 1, 1}, false)
 		sasl.gl.drawRectangle(1000, 530, 50,  - 530 + flaps_on_screen, {1, 1, 1})
 		sasl.gl.drawWideLine(990, handle_on_screen, 1060, handle_on_screen, 6, color)
@@ -263,13 +348,56 @@ function UpdateFlaps() --this is for updating flap position on eicas
 	end
 end
 
+function UpdateEicasWarnings(messages)
+	local avg_cas = (get(cas_pilot) + get(cas_copilot)) / 2
+	local avg_ra = (get(ra_pilot) + get(ra_copilot)) / 2
+	to_flaps = {1, 5, 15}
+	if get(on_ground) == 0 then
+		if avg_cas < get(stall_speed) then
+			table.insert(messages, tlen(messages) + 1, "STALL")
+		elseif avg_cas > get(max_allowable) then
+			table.insert(messages, tlen(messages) + 1, "OVERSPEED")
+		end
+	else
+		--Config warnings
+		if get(throttle_pos) > 0.5 then
+			local idx = indexOf(to_flaps, get(flaps), 1)
+			if idx == nil then
+				table.insert(messages, tlen(messages) + 1, "CONFIG FLAPS")
+			end
+			if get(park_brake_valve) == 1 then
+				table.insert(messages, tlen(messages) + 1, "CONFIG PARKING BRAKE")
+			end
+			if get(main_s_locked) == 0 then
+				table.insert(messages, tlen(messages) + 1, "CONFIG GEAR STEERING")
+			end
+		end
+	end
+	if avg_ra < 800 and get(throttle_pos) < 0.05 and gear_dn == false then
+		table.insert(messages, tlen(messages) + 1, "CONFIG GEAR")
+	end
+	if get(sys_C_press) < 900 and get(sys_R_press) < 900 then
+		table.insert(messages, tlen(messages) + 1, "STABILIZER")
+	end
+end
+
 function UpdateEicasAdvisory(messages)
-	local cur_y = 1245
-	local step = 50
-	--window positions
-	local acc = globalPropertyi("Strato/777/gear/brake_acc_in_use")
+	local avg_cas = (get(cas_pilot) + get(cas_copilot)) / 2
 	--Draw cautions
+	if get(fbw_mode) == 2 then
+		table.insert(messages, tlen(messages) + 1, "FLIGHT CONTROL MODE")
+	elseif get(fbw_mode) == 3 then
+		table.insert(messages, tlen(messages) + 1, "PRI FLIGHT COMPUTERS")
+	end
+	if avg_cas < get(manuever_speed) and avg_cas > get(stall_speed) and get(on_ground) == 0 then
+		table.insert(messages, tlen(messages) + 1, "AIRSPEED LOW")
+	end
 	UpdatePress(messages)
+	UpdateStabCutoutAdvisory(messages)
+	local door_stat = checkGearDoors()
+	if door_stat == 1 then
+		table.insert(messages, tlen(messages) + 1, "GEAR DOOR")
+	end
 	if get(acc) == 1 then
 		table.insert(messages, tlen(messages) + 1, "BRAKE SOURCE")
 	end
@@ -287,11 +415,11 @@ function UpdateEicasAdvisory(messages)
 	UpdateWindowAdvisory(messages)
 end
 
-function DisplayMessages(messages)
+function DisplayMessages(messages, offset, color)
 	local n_advisories = tlen(messages)
 	if n_advisories > 0 then
 		for i=1,n_advisories do
-			drawText(font, 850, 1245 - 50 * (i - 1), messages[i], 50, false, false, TEXT_ALIGN_LEFT, {1, 0.96, 0.07})
+			drawText(font, 850, offset - 50 * (i - 1), messages[i], 50, false, false, TEXT_ALIGN_LEFT, color)
 		end
 	end
 end
@@ -301,15 +429,20 @@ function draw()
 	flap_load_relief = globalPropertyi("Strato/777/flaps/load_relief")
 	UpdateWindows()
 	if get(battery) == 1 or IsAcConnected() == 1 then
+		local offset = 1245
+		local warnings = {}
 		UpdateCanc()
 		UpdateFlaps()
+		UpdateEicasWarnings(warnings)
+		DisplayMessages(warnings, offset, {1, 0, 0})
+		offset = offset - 50 * tlen(warnings)
 		if get(canc) == 0 or get(recall_past) == 1 then
 			local advisories = {}
-			if get(c_time) >= tmp1 + 2 then
+			if get(c_time) >= tmp1 + 1 then
 				UpdateEicasAdvisory(advisories)
 				advisories_past = advisories
 			end
-			DisplayMessages(advisories_past)
+			DisplayMessages(advisories_past, offset, {1, 0.96, 0.07})
 		end
 		if get(recall_past) == 1 and get(canc) == 1 then
 			drawText(font, 850, 680, "RECALL", 30, false, false, TEXT_ALIGN_LEFT, {1, 1, 1})
