7ed9e549449d7f412e29869374a5c797b802e631
crazytimtimtim <64425497+crazytimtimtim@users.noreply.github.com>
2022-12-12 13:35:17 -0600

    Update B777.20.fms.lua

--- plugins/xtlua/init/scripts/B777.20.fms/B777.20.fms.lua
+++ plugins/xtlua/init/scripts/B777.20.fms/B777.20.fms.lua
@@ -1,8 +1,8 @@
 --[[
 *****************************************************************************************
-* Script Name: misc
-* Author Name: Crazytimtimtim
-* Script Description: (Init) misc system code for systems that don't have enough for their own module.
+* Script Name: FMS
+* Author Name: crazytimtimtim
+* Script Description: (Init) Flight Management System code
 *****************************************************************************************
 --]]
 
