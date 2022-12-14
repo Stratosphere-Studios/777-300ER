bb72209ffa6092442abe93bc1d02fc35298694eb
crazytimtimtim <64425497+crazytimtimtim@users.noreply.github.com>
2022-12-14 08:25:21 -0600

    Add files via upload

---
+++ updateVersion.sh
@@ -0,0 +1,6 @@
+today=`date '+%Y/%m/%d %H:%M'`;
+versionid="XP1150-$today";
+versionline="fmcVersion=\"$versionid\"";
+echo $versionline >> plugins/xtlua/scripts/B777.30.xt.simconfig/version.lua
+pandoc -o README.pdf README.md
+git add --all
