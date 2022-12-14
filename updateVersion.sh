4162997f9254b58e2d93acd221559c9fa344e13c
crazytimtimtim <64425497+crazytimtimtim@users.noreply.github.com>
2022-12-14 08:26:22 -0600

    Update updateVersion.sh

--- updateVersion.sh
+++ updateVersion.sh
@@ -2,5 +2,4 @@ today=`date '+%Y/%m/%d %H:%M'`;
 versionid="XP1200-$today";
 versionline="fmcVersion=\"$versionid\"";
 echo $versionline >> plugins/xtlua/scripts/B777.30.xt.simconfig/version.lua
-pandoc -o README.pdf README.md
 git add --all
