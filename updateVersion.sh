3e45294c8d56be58a458ece9e4599662afac548e
crazytimtimtim <64425497+crazytimtimtim@users.noreply.github.com>
2023-05-18 10:21:53 -0500

    Delete updateVersion.sh

--- updateVersion.sh
+++
@@ -1,5 +0,0 @@
-today=`date '+%Y/%m/%d %H:%M'`;
-versionid="XP1200-$today";
-versionline="fmcVersion=\"$versionid\"";
-echo $versionline >> plugins/xtlua/scripts/B777.30.xt.simconfig/version.lua
-git add --all
