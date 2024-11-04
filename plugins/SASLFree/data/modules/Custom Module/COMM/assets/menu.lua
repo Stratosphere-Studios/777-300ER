d7cb279e4423df4a605df098642dfb028c8c7e19
Ker <kermenglee@gmail.com>
2024-11-05 01:07:21 +0800

    CPDLC init

---
+++ plugins/SASLFree/data/modules/Custom Module/COMM/assets/menu.lua
@@ -0,0 +1,36 @@
+-- menu.lua
+-- Language: lua
+-- Path: Custom Module\COMM\assets\menu.lua
+-- Stores all comm menu information
+
+ATC = {
+    [1] = 'Altitude\nRequest',
+    [2] = 'Route\nRequest',
+    [3] = 'Speed\nRequest',
+    [4] = 'Clearance\nRequest',
+    [5] = 'When Can We\nExpect',
+    [6] = 'Voice Contact\nRequest',
+    [7] = 'Logon / Status',
+    [8] = 'Emergency\nReport',
+    [9] = 'ATC Requested\nReports...',
+    [10] = 'Position\nReport',
+    [11] = 'Free Text\nMessage'
+}
+
+--[1] header title
+--[2] content
+
+
+Headertitle = {
+    [1] = 'Altitude Request',
+    [2] = 'Route request',
+    [3] = 'Speed request',
+    [4] = 'Clearance request',
+    [5] = 'When can we expect',
+    [6] = 'Voice contact request',
+    [7] = 'Loogon / Status',
+    [8] = 'Emergency report',
+    [9] = 'ATC Report',
+    [10] = 'Position Report',
+    [11] = 'Free Text Message'
+}
